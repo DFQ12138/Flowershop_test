92748beb84e30ccf7ebef2b227aea0ee9a4d79ae
DOU <doufengqi@outlook.com>
2022-09-14 22:19:37 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  <div class="main"><span>hello</span></div>
-  <el-row class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </el-row>
+  <HeaderCom/>
+  <router-view></router-view>
+  <FooterCom/>
 </template>
 
-<script lang="ts">
-import {defineComponent} from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+<script lang="ts" setup>
+import HeaderCom from './components/HeaderCom.vue'
+import FooterCom from './components/FooterCom.vue'
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-
-.main {
-  span {
-    color: rebeccapurple;
-  }
-
-
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
-//import 'element-plus/lib/theme-chalk/index.css'
+import router from "./routes";
 import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 // createApp(App).mount('#app')
 
 app.use(ElementPlus)
-
+app.use(router)
 app.mount('#app') // 挂载到id为app的元素上
